8798eead7ed37c4c61f721d611f52b8861db8e74
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-08-01 12:34:23 +0200

    Added code to read JSON file of shapes

--- src/university/cup/UniversityCup.java
+++ src/university/cup/UniversityCup.java
@@ -1,10 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package university.cup;
 
+package university.cup;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 /**
  *
  * @author root07
@@ -15,7 +18,39 @@ public class UniversityCup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        createShapes();
     }
     
-}
+    public static void createShapes(){
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+        
+        try (FileReader reader = new FileReader("shapes_file.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            JSONArray array = new JSONArray();
+            array.add(obj);
+            System.out.println(array);
+            
+            
+             
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void parseShapesObject(JSONObject shape) 
+    {
+        
+    }
+        
+    }
+    
+
